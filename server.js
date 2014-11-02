20ff625eabb403a3089fbe2af8ca2bd98a3eda0b
ChristianAyala <cayala@smu.edu>
2014-11-02 01:37:21 -0800

    Refactored firebase accessing to account for multiple reads. Firebase is jank

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 var express = require("express");
 var request = require("request-json");
 var Firebase = require("firebase");
+var ref = new Firebase("https://money2020-smu.firebaseio.com");
 var _ = require("lodash");
 
 var app = express();
@@ -38,10 +39,33 @@ var server = app.listen(3000, function() {
 function getGuid(params, callback) {
     var card_hash = params.card_hash;
     var user_fullname = params.user_fullname;
-    var ref = new Firebase("https://money2020-smu.firebaseio.com");
     var userCollection = ref.child("users");
-    var guid = null;
+    var GUID = null;
     console.log("Retrieving guid");
+
+    ref.once("value", function(userList) {
+        if (userList.val() == null) {
+            GUID = storeNewUser(card_hash, userCollection);
+            userList = {};
+            userList[GUID] = {card_hash: [card_hash]};
+        } else {
+            userList = userList.val().users;
+        }
+        _.forOwn(userList, function(userData, guid) {
+            if (userData.card_hash.indexOf(card_hash) > -1) {
+                console.log ("Found user hash: " + guid);
+                GUID = guid;
+                if (user_fullname != null && !userData.user_fullname) {
+                    updateUserName(GUID, user_fullname, userCollection);
+                }
+                addIPAddress(GUID, params.ip, userData, userCollection);
+                callback(GUID);
+            }
+        });
+    });
+
+
+    /*
     userCollection.on("child_added", function(user) {
         var userData = user.val();
 
@@ -61,13 +85,13 @@ function getGuid(params, callback) {
             guid = storeNewUser(card_hash, userCollection);
 
         }
-    });
+    });*/
 }
 
 function storeNewUser(card_hash, userCollection) {
     var guid = require("node-uuid").v1();
     console.log("Storing new user: " + guid);
-    userCollection.child(guid).set({"card_hash": [card_hash]});
+    userCollection.child(guid).update({"card_hash": [card_hash]});
     return guid;
 }
 
